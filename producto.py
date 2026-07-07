028f07c26407a37ed3a0f9c5f71f32093734d324
Pamela Arias <amg.sosa@yavirac.edu.ec>
2026-07-07 09:46:18 -0500

    producto

--- producto.py
+++ producto.py
@@ -2,3 +2,7 @@ class Producto:
     def __init__(self, nombre, precio):
         self.nombre = nombre
         self.precio = precio
+
+    def calcular_descuento(self, porcentaje):
+        descuento = self.precio * (porcentaje / 100)
+        return self.precio - descuento
