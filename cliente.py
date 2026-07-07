7f419e4fe732b24d4c4d6bf9d6f58046152bf237
jltvalencia <jlt.valencia@yavirac.edu.ec>
2026-07-07 10:12:59 -0500

    cliente final

--- cliente.py
+++ cliente.py
@@ -1,7 +1,9 @@
 class Cliente:
-    def __init__(self, nombre, email):
+    def __init__(self, cedula, nombre, email, telefono):
+        self.cedula = cedula
         self.nombre = nombre
         self.email = email
+        self.telefono = telefono
 
     def mostrar_perfil(self):
-        return f"Cliente: {self.nombre} - Email: {self.email}"
+        return f"Cliente: {self.nombre} | Contacto: {self.email}"
